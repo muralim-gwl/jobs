21ce1cd888f92ae2c847777eaf9d0bb11f644d73
murali m <muralim4242@gmail.com>
2017-05-04 18:24:36 +0530

    Initial project structure

---
+++ screens/ReviewScreen.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import {View, Text, Platform} from "react-native";
+import {Button} from "react-native-elements";
+
+class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: 'Review Jobs',
+    header: ({navigate}) => {
+      return {
+        right: (<Button title='Setting' onPress={() => navigate('setting')} backgroundColor='rgba(0,0,0,0)' color='rgba(0,122,255,1)'/>),
+        style: {
+          marginTop: Platform.OS === "android"
+            ? 24
+            : 0
+        }
+      };
+    }
+  }
+
+    render() {
+      return (
+        <View>
+          <Text>ReviewScreen</Text>
+          <Text>ReviewScreen</Text>
+          <Text>ReviewScreen</Text>
+          <Text>ReviewScreen</Text>
+          <Text>ReviewScreen</Text>
+          <Text>ReviewScreen</Text>
+        </View>
+      )
+    }
+  }
+
+  export default ReviewScreen;
